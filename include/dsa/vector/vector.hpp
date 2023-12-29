374db4b6aefe35ef810826c97ae22dee60f2ede9
Reinaldo Molina <rmolin88@gmail.com>
2023-12-28 21:25:20 -0500

    feat: move away from malloc
    - Implemented size
    - Implemented array operator
    - Implemented ctors

--- include/dsa/vector/vector.hpp
+++ include/dsa/vector/vector.hpp
@@ -1,5 +1,6 @@
 #include <concepts>
 #include <memory>
+#include <iostream>
 
 template <typename T>
 concept Vector = requires(T a, T b) {
@@ -14,26 +15,59 @@ template <Vector T> class vector {
   T *data_{};
   size_t capacity_{};
   size_t size_{};
-  size_t data_size_{};
 
   bool insert_generic(size_t index);
 
 public:
-  vector() { data_size_ = sizeof(T); }
-  explicit vector(size_t capacity) : capacity_(capacity) {
-    data_size_ = sizeof(T);
-    data_ = (T *)malloc(capacity_ * data_size_);
+  vector() = default;
+  explicit vector(int capacity) {
+    if (capacity <= 0) return;
+    capacity_ = capacity;
+    data_ = new T[capacity_];
+    std::cout << "allocated '" << capacity << "' elements from capacity\n";
+  }
+  vector(std::initializer_list<T> init_list) {
+    size_ = init_list.size();
+    capacity_ = size_;
+    data_ = new T[capacity_];
+    std::copy(init_list.begin(), init_list.end(), data_);
+    std::cout << "allocated '" << size_ << "' bytes from initializer_list\n";
   }
   ~vector() {
     if (data_)
-      free(data_);
+    {
+      delete[] data_;
+      std::cout << "freeing memory\n";
+    }
   }
+
+  // TODO: realloc
+  // TODO: Figure out why they are not being called
+  vector(const T &&other) {
+    std::cout << "move ctor!!!\n";
+  }
+  vector(const T &other) {
+    std::cout << "copy ctor!!!\n";
+    if (other.capacity_ > 0) {
+      capacity_ = other.capacity_;
+      data_ = new T[capacity_];
+    }
+    if (other.size_ > 0) {
+      std::copy(other.begin(), other.end(), data_);
+      size_ = other.size_;
+    }
+  }
+  T &operator=(const T &other) = delete;
+  T &&operator=(const T &&other) = delete;
+  T operator[](int idx) const { return data_[idx]; }
 
   bool insert(T &&el, size_t index);
   bool insert(const T &el, size_t index);
 
-  // Not copiable
-  vector(const T &other) = delete;
-  T &operator=(const T &other) = delete;
-  T &&operator=(const T &&other) = delete;
+  T* begin() const { return &data_[0]; }
+  const T* cbegin() const { return &data_[0]; }
+  T* end() const { return &data_[size_]; }
+  const T* cend() const { return &data_[size_]; }
+
+  size_t size() const { return size_; }
 };
--- tests/test_vector.cxx
+++ tests/test_vector.cxx
@@ -1,6 +1,17 @@
-#include <gtest/gtest.h>
 #include "dsa/vector/vector.hpp"
+#include <gtest/gtest.h>
+#include <iostream>
 
 TEST(mytest, this_test) {
-  vector<int> v{static_cast<size_t>(8)};
+  vector<float> v(10);
+  vector<float> f{8.8f, 7.0f, 79.9f};
+  std::cout << "f =\n";
+  for (auto d : f)
+    std::cout << "\td = '" << d << "'\n";
+  const auto f2(std::move(f));
+  std::cout << "f2 =\n";
+  for (auto d : f2)
+    std::cout << "\td = '" << d << "'\n";
+  for (int i = 0; i < f2.size(); i++)
+    std::cout << "\td = '" << f2[i] << "'\n";
 }
