cef9c583bcc1cf54b7827aa0d16a450843fc52b0
Reinaldo Molina <rmolin88@gmail.com>
2023-12-30 08:55:54 -0500

    fix: copy/move semantics
    - Tests pass
    - Formatting

--- include/dsa/vector/vector.hpp
+++ include/dsa/vector/vector.hpp
@@ -1,6 +1,6 @@
 #include <concepts>
-#include <memory>
 #include <iostream>
+#include <memory>
 
 template <typename T>
 concept Vector = requires(T a, T b) {
@@ -21,7 +21,8 @@ template <Vector T> class vector {
 public:
   vector() = default;
   explicit vector(int capacity) {
-    if (capacity <= 0) return;
+    if (capacity <= 0)
+      return;
     capacity_ = capacity;
     data_ = new T[capacity_];
     std::cout << "allocated '" << capacity << "' elements from capacity\n";
@@ -34,19 +35,23 @@ public:
     std::cout << "allocated '" << size_ << "' bytes from initializer_list\n";
   }
   ~vector() {
-    if (data_)
-    {
+    if (data_) {
       delete[] data_;
       std::cout << "freeing memory\n";
     }
   }
 
   // TODO: realloc
-  // TODO: Figure out why they are not being called
-  vector(const T &&other) {
+  vector(vector<T> &&other) {
     std::cout << "move ctor!!!\n";
+    size_ = other.size_;
+    capacity_ = other.capacity_;
+    data_ = other.data_;
+    other.size_ = 0;
+    other.capacity_ = 0;
+    other.data_ = 0;
   }
-  vector(const T &other) {
+  vector(const vector<T> &other) {
     std::cout << "copy ctor!!!\n";
     if (other.capacity_ > 0) {
       capacity_ = other.capacity_;
@@ -57,17 +62,19 @@ public:
       size_ = other.size_;
     }
   }
-  T &operator=(const T &other) = delete;
-  T &&operator=(const T &&other) = delete;
+
+  // TODO
+  vector<T> &operator=(const vector<T> &other) = delete;
+  vector<T> &operator=(vector<T> &&other) = delete;
   T operator[](int idx) const { return data_[idx]; }
 
   bool insert(T &&el, size_t index);
   bool insert(const T &el, size_t index);
 
-  T* begin() const { return &data_[0]; }
-  const T* cbegin() const { return &data_[0]; }
-  T* end() const { return &data_[size_]; }
-  const T* cend() const { return &data_[size_]; }
+  T *begin() const { return &data_[0]; }
+  const T *cbegin() const { return &data_[0]; }
+  T *end() const { return &data_[size_]; }
+  const T *cend() const { return &data_[size_]; }
 
   size_t size() const { return size_; }
 };
--- tests/test_vector.cxx
+++ tests/test_vector.cxx
@@ -3,15 +3,21 @@
 #include <iostream>
 
 TEST(mytest, this_test) {
-  vector<float> v(10);
+  vector<int> v(10);
   vector<float> f{8.8f, 7.0f, 79.9f};
   std::cout << "f =\n";
   for (auto d : f)
     std::cout << "\td = '" << d << "'\n";
   const auto f2(std::move(f));
   std::cout << "f2 =\n";
-  for (auto d : f2)
-    std::cout << "\td = '" << d << "'\n";
   for (int i = 0; i < f2.size(); i++)
     std::cout << "\td = '" << f2[i] << "'\n";
+  const auto f3(f2);
+  std::cout << "f3 =\n";
+  for (auto d : f3)
+    std::cout << "\td = '" << d << "'\n";
+  std::cout << "f (was moved)=\n";
+  for (auto d : f)
+    std::cout << "\td = '" << d << "'\n";
+  // f3 = f;
 }
