1abf1dd21a4d4cc382783fdb563da87db683aad0
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-05-02 05:37:11 -0400

    tests: more modular

--- tests/test_vector.cxx
+++ tests/test_vector.cxx
@@ -9,37 +9,52 @@ TEST(vector, constructor)
   vector<int> v(10);
   // Constructor with initializer list
   vector<float> f{ 8.8f, 7.0f, 79.9f };
+}
 
+TEST(vector, iterators)
+{
+  vector<float> f{ 8.8f, 7.0f, 79.9f };
   // Iterators
   std::cout << "f =\n";
   for (auto d : f)
     std::cout << "\td = '" << d << "'\n";
+}
 
+TEST(vector, move_ctor)
+{
+  vector<float> f{ 8.8f, 7.0f, 79.9f };
   // Move constructor
   const auto f2(std::move(f));
   std::cout << "f2 =\n";
   for (int i = 0; i < f2.size(); i++)
     std::cout << "\td = '" << f2[i] << "'\n";
+}
 
+TEST(vector, copy_ctor)
+{
+  vector<float> f{ 8.8f, 7.0f, 79.9f };
   // Copy constructor
-  auto f3(f2);
+  auto f3(f);
   std::cout << "f3 =\n";
   for (auto d : f3)
     std::cout << "\td = '" << d << "'\n";
   std::cout << "f (was moved)=\n";
   for (auto d : f)
     std::cout << "\td = '" << d << "'\n";
+}
 
-  // Copy assignment
+TEST(vector, copy_assignment)
+{
+  vector<float> f{ 8.8f, 7.0f, 79.9f };
   vector<float> f4{ 1.0f, 2.0f, 3.0f };
   std::cout << "f4 originally =\n";
   for (auto d : f4)
     std::cout << "\td = '" << d << "'\n";
-  f4 = f3;
+  f4 = f;
   std::cout << "f4 after the copy =\n";
   for (auto d : f4)
     std::cout << "\td = '" << d << "'\n";
-  std::cout << "f3\n";
-  for (auto d : f3)
+  std::cout << "f\n";
+  for (auto d : f)
     std::cout << "\td = '" << d << "'\n";
 }
