0ca5d4ad1d58dab7e27a4563ed046bb397b02fd8
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-05-02 21:35:03 -0400

    tests: automated
    - Before was a bunch of printfs

--- tests/test_vector.cxx
+++ tests/test_vector.cxx
@@ -9,16 +9,21 @@ TEST(vector, constructor)
   vector<int> v(10);
   // Constructor with initializer list
   vector<float> f{ 8.8f, 7.0f, 79.9f };
+  EXPECT_EQ(f.size(), 3);
+  EXPECT_EQ(f[0], 8.8f);
+  EXPECT_EQ(f[1], 7.0f);
+  EXPECT_EQ(f[2], 79.9f);
   vector<double> d{};
+  EXPECT_TRUE(d.empty());
 }
 
 TEST(vector, iterators)
 {
   vector<float> f{ 8.8f, 7.0f, 79.9f };
-  // Iterators
-  std::cout << "f =\n";
+  float arr[] = {8.8f, 7.0f, 79.9f};
+  int i = 0;
   for (auto d : f)
-    std::cout << "\td = '" << d << "'\n";
+    EXPECT_EQ(d, arr[i++]);
 }
 
 TEST(vector, move_ctor)
@@ -26,9 +31,11 @@ TEST(vector, move_ctor)
   vector<float> f{ 8.8f, 7.0f, 79.9f };
   // Move constructor
   const auto f2(std::move(f));
-  std::cout << "f2 =\n";
-  for (int i = 0; i < f2.size(); i++)
-    std::cout << "\td = '" << f2[i] << "'\n";
+  EXPECT_TRUE(f.empty()); // f should be empty after being moved
+  EXPECT_EQ(f2.size(), 3);
+  EXPECT_EQ(f2[0], 8.8f);
+  EXPECT_EQ(f2[1], 7.0f);
+  EXPECT_EQ(f2[2], 79.9f);
 }
 
 TEST(vector, copy_ctor)
@@ -36,42 +43,33 @@ TEST(vector, copy_ctor)
   vector<float> f{ 8.8f, 7.0f, 79.9f };
   // Copy constructor
   auto f3(f);
-  std::cout << "f3 =\n";
-  for (auto d : f3)
-    std::cout << "\td = '" << d << "'\n";
-  std::cout << "f (was moved)=\n";
-  for (auto d : f)
-    std::cout << "\td = '" << d << "'\n";
+  EXPECT_EQ(f.size(), 3); // f should remain unchanged after being copied
+  EXPECT_EQ(f3.size(), 3);
+  EXPECT_EQ(f3[0], 8.8f);
+  EXPECT_EQ(f3[1], 7.0f);
+  EXPECT_EQ(f3[2], 79.9f);
 }
 
 TEST(vector, copy_assignment)
 {
   vector<float> f{ 8.8f, 7.0f, 79.9f };
   vector<float> f4{ 1.0f, 2.0f, 3.0f };
-  vector<float> f5{};
-  std::cout << "f4 originally =\n";
-  for (auto d : f4)
-    std::cout << "\td = '" << d << "'\n";
   f4 = f;
-  std::cout << "f4 after the copy =\n";
-  for (auto d : f4)
-    std::cout << "\td = '" << d << "'\n";
-  std::cout << "f\n";
-  for (auto d : f)
-    std::cout << "\td = '" << d << "'\n";
-  f4 = f5;
+  EXPECT_EQ(f.size(), 3); // f should remain unchanged after being copied
+  EXPECT_EQ(f4.size(), 3);
+  EXPECT_EQ(f4[0], 8.8f);
+  EXPECT_EQ(f4[1], 7.0f);
+  EXPECT_EQ(f4[2], 79.9f);
 }
 
 TEST(vector, move_assignment)
 {
   vector<float> f{ 8.8f, 7.0f, 79.9f };
   vector<float> f4{ 1.0f, 2.0f, 3.0f };
-
-  std::cout << "f4 originally =\n";
-  for (auto d : f4)
-    std::cout << "\td = '" << d << "'\n";
   f4 = std::move(f);
-  std::cout << "f4 after the copy =\n";
-  for (auto d : f4)
-    std::cout << "\td = '" << d << "'\n";
+  EXPECT_TRUE(f.empty()); // f should be empty after being moved
+  EXPECT_EQ(f4.size(), 3);
+  EXPECT_EQ(f4[0], 8.8f);
+  EXPECT_EQ(f4[1], 7.0f);
+  EXPECT_EQ(f4[2], 79.9f);
 }
